8af834a6cad9afee03975c42c387b8eb9b3037d2
DanielBerns <daniel.w.berns@gmail.com>
2024-03-06 19:15:33 -0300

    user login ui

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 A private web
 
 ## How to run
-Note that we use .flaskenv
+Note that we use .flaskenv for configuring FLASK_APP
 
 $ flask run
--- server/application/routes.py
+++ server/application/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from application import app
+from application.forms import LoginForm
 
 @app.route('/')
 def index():
@@ -14,5 +15,20 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('page.html.jinja', title='Home', user=user, posts=posts)    
+    return render_template(
+        'index.html.jinja', 
+        title='Home', 
+        header='Home',
+        user=user, 
+        posts=posts
+    )    
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data},'
+              f' remember_me={form.remember_me.data}'
+        )
+        return redirect('/')
+    return render_template('login.html.jinja', title='Sign In', form=form)
